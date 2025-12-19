cff9cdbaea316e375036df9acb90ee9f5d4317a9
Nuaiman Ashiq <ashiqmuaiman@gmail.com>
2025-12-19 23:35:18 +0600

    commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(resultChannel chan string, jobChannel chan string) {
+	for job := range jobChannel {
+		time.Sleep(50 * time.Millisecond)
+		fmt.Printf("Image processed: %s \n", job)
+
+		resultChannel <- job
+	}
+}
+
+func main() {
+	images := []string{
+		"image_1.png",
+		"image_2.png",
+		"image_3.png",
+		"image_4.png",
+	}
+
+	var wg sync.WaitGroup
+	resultChannel := make(chan string, 2)
+	jobChannel := make(chan string, len(images))
+
+	totalWorkers := 2
+	startTime := time.Now()
+
+	for i:=1;i<=totalWorkers; i++{
+		wg.Go(func() {worker(resultChannel,jobChannel)})
+	}
+	
+	go func ()  {
+		wg.Wait()
+		close(resultChannel)
+	} ()
+
+	for i:=0; i<len(images); i++ {
+		jobChannel <- images[i]
+	}
+
+	close(jobChannel)
+
+	for result := range resultChannel {
+		fmt.Printf("recieved %s \n", result)
+	}
+
+	fmt.Printf("Total Time Taken: %s \n", time.Since(startTime))
+}
